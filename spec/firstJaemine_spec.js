8c06807367624f19ad9fa8c7a896c0fe5d46981d
David Erick Avalos Guerra <david avalos@tiempodevelopment.com>
2019-05-06 11:20:25 -0500

    First Commit

---
+++ spec/firstJaemine_spec.js
@@ -0,0 +1,10 @@
+/**
+ * @description 
+ * @author David Avalos
+ * @lastUpdate
+ */
+describe("suite01", function(){
+    it("test01", function(){
+        console.log("Output test01");
+    });
+});
